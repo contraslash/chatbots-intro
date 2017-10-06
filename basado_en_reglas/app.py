58876763ccd3ed7bdc8abe1ca154405705e9930c
Mauricio Collazos <ma0@contraslash.com>
2017-10-05 22:50:14 -0500

    added rule bassed chatbot

---
+++ basado_en_reglas/app.py
@@ -0,0 +1,69 @@
+__author__ = "Mauricio Collazos"
+
+tasks = list()
+
+
+def extraction(raw_text):
+    entities = {
+        "indexes": list(),
+        "tasks": list()
+    }
+    words = raw_text.split()
+
+    # Extract indexes:
+    for word in words:
+        try:
+            task_index = int(word)
+            entities["indexes"].append(task_index)
+        except ValueError:
+            pass
+    # Extract Taks:
+    if ":" in raw_text:
+        tasks = raw_text[raw_text.index(":")+1:]
+        entities["tasks"] = tasks.split(",")
+    return entities
+
+
+def classification(raw_text):
+    if "listar" in raw_text:
+        return "list"
+    if "mostrar" in raw_text:
+        return "show"
+    if "crear" in raw_text:
+        return "create"
+    if "eliminar" in raw_text:
+        return "delete"
+    return ""
+
+
+def production(intent, entities):
+    if intent == "list":
+        return "Todas las tareas son: {}".format(
+            "{}".format(zip(range(len(tasks)), tasks))
+        )
+    elif intent == "show":
+        return "Las tareas solicitadas son: {}".format(
+            "\n".join(
+                [tasks[i] for i in entities["indexex"]]
+            )
+        )
+    elif intent == "create":
+        [tasks.append(task) for task in entities["tasks"]]
+        return "Tareas agregadas"
+    elif intent == "delete":
+        [tasks.pop(index) for index in entities["indexes"]]
+        return "Tareas eliminadas"
+    else:
+        "No entiendo lo que quieres decir"
+
+
+if __name__ == "__main__":
+    print("Bienvenido a tu lista de tareas, que deseas hacer")
+    raw_text = raw_input()
+    while raw_text != "salir":
+        entities = extraction(raw_text)
+        intent = classification(raw_text)
+        response = production(intent, entities)
+        print("que otra cosa deseas hacer")
+        print(response) if response is not None else ""
+        raw_text = raw_input()
